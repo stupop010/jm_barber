8af449f11f19a4f92cce91da823e24cb3a46b76e
stuart <stupop010@gmail.com>
2020-07-23 11:46:11 +0100

    added link

--- components/AppointmentBtn/AppointmentBtn.js
+++ components/AppointmentBtn/AppointmentBtn.js
@@ -1,5 +1,10 @@
+import Link from "next/link";
 import styles from "./AppointmentBtn.module.scss";
 
 export default function AppointmentBtn() {
-  return <a className={styles.btn}>appointment now</a>;
+  return (
+    <Link href="/appointment">
+      <a className={styles.btn}>appointment now</a>;
+    </Link>
+  );
 }
